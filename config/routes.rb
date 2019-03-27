cd6d626b6bd0c25a685b2f4034696550475234bf
Shawn Kearney <shawnpk@ineptsoftware.com>
2019-03-27 14:12:59 -0600

    Add name to users table and devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
+
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
+  
   root 'pages#index'
 end
