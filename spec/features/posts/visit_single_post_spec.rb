01acf19bc77770be0ee635778dc094de1ee3a1f5
Shawn Kearney <shawnpk@ineptsoftware.com>
2019-03-28 12:25:30 -0600

    Add feature spec to view single post

---
+++ spec/features/posts/visit_single_post_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature 'Visit single post', type: :feature do
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
+
+  scenario 'User visits a single post from the homepage', js: true do
+    post
+
+    visit root_path
+    page.find('.single-post-card').click
+
+    expect(page).to have_selector('body .modal')
+
+    page.find('.interested a').click
+
+    expect(page).to have_selector('#single-post-content p', text: post.content)
+  end
+end
