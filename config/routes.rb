0c6a0119ce37852ebf503a7ec5f2233b552c4493
Shawn Kearney <shawnpk@ineptsoftware.com>
2019-03-28 08:29:36 -0600

    Display posts on the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,13 @@ Rails.application.routes.draw do
     get 'signup', to: 'devise/registrations#new'
   end
 
+  resources :posts do
+    collection do
+      get 'hobby'
+      get 'study'
+      get 'team'
+    end
+  end
+
   root 'pages#index'
 end
